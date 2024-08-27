399020c6e5e203e2e152efc4034092bc7e5e5f14
Dalia Tavizon-Dykstra <83558848+dtavizondykstra@users.noreply.github.com>
2024-08-27 11:30:14 -0600

    updating the workflow files necessary for publishing to PyPi

---
+++ run.sh
@@ -0,0 +1,82 @@
+#!/bin/bash -x
+
+set -e
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function install {
+    python -m pip install --upgrade pip
+    python -m pip install --editable "$THIS_DIR/[dev]"
+}
+
+function lint {
+    pre-commit run --all-files
+}
+
+function lint:ci {
+    SKIP=no-commit-to-branch pre-commit run --all-files
+}
+
+function build {
+    python -m build --sdist --wheel "$THIS_DIR/"
+}
+
+function release:test {
+    lint
+    clean
+    build
+    publish:test
+}
+
+function release:prod {
+    release:test
+    publish:prod
+}
+
+function publish:test {
+    try-load-dotenv || true
+    twine upload dist/* \
+        --repository testpypi \
+        --password="$TEST_PYPI_TOKEN"
+}
+
+function publish:prod {
+    try-load-dotenv || true
+    twine upload dist/* \
+        --repository pypi \
+        --username=__token__ \
+        --password="$PROD_PYPI_TOKEN"
+}
+
+function clean {
+    rm -rf dist build
+    find . \
+      -type d \
+      \( \
+        -name "*cache*" \
+        -o -name "*.dist-info" \
+        -o -name "*.egg-info" \
+      \) \
+      -not -path "./venv/*" \
+      -exec rm -r {} +
+}
+
+function try-load-dotenv {
+    if [ ! -f "$THIS_DIR/.env" ]; then
+        echo "no .env file found"
+        return 1
+    fi
+
+    while read -r line; do
+        export "$line"
+    done < <(grep -v '^#' "$THIS_DIR/.env" | grep -v '^$')
+}
+
+function help {
+    echo "$0 <task> <args>"
+    echo "Tasks:"
+    compgen -A function | cat -n
+}
+
+TIMEFORMAT="Task completed in %3lR"
+time ${@:-help}
